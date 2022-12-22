b3d5e122543de98ae6e6066f2ba14274b0f5e35b
Sarah <sarah@MacBook-Pro.hitronhub.home>
2022-12-22 13:48:40 -0700

    project setup, including additon of tailwind and background from old site

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet'
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    let num = 25;
+
+    for (let i=0; i < num; i++) {
+      let xPos = Math.floor((Math.random() * 100)+1);
+      let speed = Math.floor((Math.random() * 50)+10);
+      let diameter = Math.floor((Math.random() * 50)+5);
+  
+      let background = document.getElementById("background")
+      if (background) {
+        background.innerHTML += ('<div class="bubble"  style="left: '+ xPos +'%;\
+                        -webkit-animation-duration: '+ speed +'s;\
+                        animation-duration:'+ speed +'s; \
+                        width:'+diameter+'px; \
+                      height:'+diameter+'px;"  ></div>');
+      }
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sarah Todd | Frontend Developer</title>
+        <link rel="canonical" href="http://satodd.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"></link>
+      </Helmet>
+      <div className='relative h-screen w-full overflow-hidden bg-coral'>
+        <div className="absolute inset-0 -z-[1]"></div>
+        <header className='absolute top-0 p-8 bg-white'>
+          <h1 className="pb-1 text-6xl text-coral font-bold">Sarah Todd</h1>
+          <p className='text-2xl'>Frontend Developer</p>
+        </header>
+      </div>
+    </>
   );
 }
 
