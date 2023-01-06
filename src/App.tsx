a96da46e2c0a4c82532b1c951c4804a59b106da0
Sarah <saatodd@gmail.com>
2023-01-06 10:20:03 -0700

    reworking of bubbles

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,19 @@ function App() {
   
       let background = document.getElementById("background")
       if (background) {
-        background.innerHTML += ('<div class="bubble"  style="left: '+ xPos +'%;\
-                        -webkit-animation-duration: '+ speed +'s;\
-                        animation-duration:'+ speed +'s; \
-                        width:'+diameter+'px; \
-                      height:'+diameter+'px;"  ></div>');
+        const bubble = document.createElement("div");
+        bubble.className = "bubble"
+        bubble.style.setProperty("left", String(xPos + '%'))
+        bubble.style.setProperty("animation-duration", speed +'s')
+        bubble.style.setProperty("-webkit-animation-duration", speed +'s')
+        bubble.style.setProperty("width", diameter +'px')
+        bubble.style.setProperty("height", diameter +'px')
+        
+        background.appendChild(bubble)
+
       }
     }
-  })
+  }, [])
 
   return (
     <>
@@ -37,7 +42,8 @@ function App() {
         <div id="background" className="absolute inset-0 z-0"></div>
         <header className='absolute top-0 p-8 bg-white'>
           <h1 className="pb-1 text-6xl text-coral font-bold">Sarah Todd</h1>
-          <p className='text-2xl'>Frontend Developer</p>
+          <p className='mb-4 text-2xl'>Frontend Developer</p>
+          <p> Now looking for work!</p>
         </header>
       </div>
     </>
