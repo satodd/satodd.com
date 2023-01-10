5428376cb9f4000f54f591e2421823c96995f5be
Sarah <saatodd@gmail.com>
2023-01-10 10:33:43 -0700

    updating homepage to fix 404 error, adding in email, github and linkedin links

--- src/App.tsx
+++ src/App.tsx
@@ -46,12 +46,12 @@ function App() {
         <div id="background" className="absolute inset-0 z-0"></div>
         <header className='absolute top-0 p-8 bg-white'>
           <h1 className="pb-1 text-6xl text-coral font-bold">Sarah Todd</h1>
-          <p className='mb-4 text-2xl'>Frontend Developer</p>
+          <p className='mb-4 text-xl'>Frontend Developer</p>
           <p className='mb-4'> Now looking for work!</p>
-          <div className='flex flex-row gap-x-3'>
+          <div className='flex flex-row items-center gap-x-3'>
             <a href="https://www.linkedin.com/in/saatodd/">
                 <svg
-                  className='w-6'
+                  className='w-6 hover:opacity-80 transition duration-300'
                   data-name="Layer 1"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 122.88 122.31"
@@ -75,7 +75,7 @@ function App() {
             <a href="https://github.com/satodd">
               <svg
                 id="Layer_1"
-                className='w-6'
+                className='w-6 hover:opacity-80 transition duration-300'
                 xmlns="http://www.w3.org/2000/svg"
                 x={0}
                 y={0}
@@ -111,12 +111,11 @@ function App() {
             <a href="mailto:saatodd@gmail.com">
               <svg
                 data-name="Layer 1"
-                className='w-6'
+                className='w-6 hover:opacity-80 transition duration-300'
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 122.88 122.88"
+                viewBox="0 0 122.88 88.86"
               >
-                <title>{"email-round"}</title>
-                <path d="M61.44 0A61.46 61.46 0 1 1 18 18 61.21 61.21 0 0 1 61.44 0ZM32.22 79.39 52.1 59.46 32.22 43.25v36.14Zm22.07-18.15-20.5 20.55h55.12L69.33 61.24l-6.46 5.51a1.42 1.42 0 0 1-1.8 0l-6.78-5.53Zm17.18-1.82 19.19 20.13V43.07L71.47 59.42ZM34 41.09l27.9 22.76 26.75-22.76Zm65.4-17.64a53.72 53.72 0 1 0 15.74 38 53.56 53.56 0 0 0-15.74-38Z" />
+                <path d="M7.05 0h108.78a7.07 7.07 0 0 1 7 7.05v74.76a7 7 0 0 1-1.22 4 2.78 2.78 0 0 1-.66 1 2.62 2.62 0 0 1-.66.46 7 7 0 0 1-4.51 1.65H7.05a7.07 7.07 0 0 1-7-7V7.05a7.07 7.07 0 0 1 7-7.05Zm-.3 78.84 36.78-38.22L6.75 9.54v69.3Zm42.32-33.45L9.77 83.45h103L75.22 45.39l-11 9.21a2.7 2.7 0 0 1-3.45 0l-11.7-9.21Zm31.6-4.84 35.46 38.6V9.2L80.67 40.55ZM10.21 5.41 62.39 47.7l49.88-42.29Z" />
               </svg>
             </a>
           </div>
